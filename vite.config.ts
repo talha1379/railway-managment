2a357b3b22921cba59527d4fda0b54018df59bf2
talha1379 <talhaahmad1379@gmail.com>
2026-05-11 21:39:10 +0500

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
-import tanstackStart from "@tanstack/react-start/plugin/vite";
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), tanstackStart()],
+  plugins: [react(), tsconfigPaths()],
 });
