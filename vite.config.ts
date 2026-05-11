739830f19d24aad94bdd2e4c9854d42ff874c673
talha1379 <talhaahmad1379@gmail.com>
2026-05-11 22:03:49 +0500

    fix issur

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import tanstackStart from "@tanstack/react-start/plugin/vite";
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), tanstackStart()],
 });
